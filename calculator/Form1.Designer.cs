083a57632adb14af7a69f9917925f4c4dea8a3fa
samodhir <samodhiruwangi@gmail.com>
2018-11-14 22:27:58 +0530

    enable , disable methods added to the system

--- calculator/Form1.Designer.cs
+++ calculator/Form1.Designer.cs
@@ -71,16 +71,18 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "ON";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(13, 70);
+            this.button2.Location = new System.Drawing.Point(13, 71);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(48, 31);
             this.button2.TabIndex = 2;
             this.button2.Text = "OFF";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -121,6 +123,7 @@
             this.button6.TabIndex = 6;
             this.button6.Text = "7";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -131,6 +134,7 @@
             this.button7.TabIndex = 7;
             this.button7.Text = "8";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -141,6 +145,7 @@
             this.button8.TabIndex = 8;
             this.button8.Text = "9";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -172,6 +177,7 @@
             this.button11.TabIndex = 11;
             this.button11.Text = "5";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // button12
             // 
@@ -182,6 +188,7 @@
             this.button12.TabIndex = 12;
             this.button12.Text = "6";
             this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
             // 
             // button13
             // 
--- calculator/Form1.cs
+++ calculator/Form1.cs
@@ -17,6 +17,57 @@ namespace calculator
             InitializeComponent();
         }
 
+        public void disable()//create one method to disable 
+        {
+            textBox1.Enabled = false;
+            button1.Show();
+            button2.Hide();
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+            button8.Enabled = false;
+            button9.Enabled = false;
+            button10.Enabled = false;
+            button11.Enabled = false;
+            button12.Enabled = false;
+            button13.Enabled = false;
+            button14.Enabled = false;
+            button15.Enabled = false;
+            button16.Enabled = false;
+            button17.Enabled = false;
+            button18.Enabled = false;
+            button19.Enabled = false;
+            button20.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        public void enable()
+        {
+            textBox1.Enabled = true;
+            button1.Hide();
+            button2.Show();
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+            button9.Enabled = true;
+            button10.Enabled = true;
+            button11.Enabled = true;
+            button12.Enabled = true;
+            button13.Enabled = true;
+            button14.Enabled = true;
+            button15.Enabled = true;
+            button16.Enabled = true;
+            button17.Enabled = true;
+            button18.Enabled = true;
+            button19.Enabled = true;
+            button20.Enabled = true;
+            button3.Enabled = true;
+        }
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
@@ -62,6 +113,51 @@ namespace calculator
             //displayed 4 in blue when click the 4 botton
             textBox1.Text = textBox1.Text + 4;
             textBox1.ForeColor = Color.Blue;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            //displayed 5 in blue when click the 5 botton
+            textBox1.Text = textBox1.Text + 5;
+            textBox1.ForeColor = Color.Blue;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            //displayed 6 in blue when click the 6 botton
+            textBox1.Text = textBox1.Text + 6;
+            textBox1.ForeColor = Color.Blue;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //displayed 7 in blue when click the 7 botton
+            textBox1.Text = textBox1.Text + 7;
+            textBox1.ForeColor = Color.Blue;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //displayed 8 in blue when click the 1 botton
+            textBox1.Text = textBox1.Text + 8;
+            textBox1.ForeColor = Color.Blue;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //displayed 9 in blue when click the 9 botton
+            textBox1.Text = textBox1.Text + 9;
+            textBox1.ForeColor = Color.Blue;
+        }
+
+        private void button2_Click(object sender, EventArgs e)//OFF button
+        {
+            disable();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            enable();
         }
     }
 }
